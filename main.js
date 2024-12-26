a050e3ac777608c1274f9df1315bb59d7a6732b7
manar mustafa <abuhaitmanar@gmail.com>
2024-12-26 23:20:03 +0300

    new

---
+++ main.js
@@ -0,0 +1,32 @@
+const signIn =  document.getElementById("signIn").addEventListener("click", function(){
+
+    var register = document.getElementById("register").classList.add("hidden");
+    var login = document.getElementById("login").classList.remove("hidden");
+
+
+});
+
+
+const signUp =  document.getElementById("signUp").addEventListener("click", function(){
+    var register = document.getElementById("register").classList.remove("hidden");
+    var login = document.getElementById("login").classList.add("hidden");
+
+});
+
+
+const form1 = document.getElementById("form1").addEventListener("click", function(){
+    var register = document.getElementById("register").classList.add("hidden");
+    var login = document.getElementById("login").classList.remove("hidden");
+
+});
+
+
+const form2 = document.getElementById("form2").addEventListener("click", function(){
+    var register = document.getElementById("register").classList.remove("hidden");
+    var login = document.getElementById("login").classList.add("hidden");
+
+});
+
+const menu = document.getElementById("menu-toggle").addEventListener("click",function(){
+     var toggleMenu = document.getElementById("menu").classList.toggle("hidden");
+});
